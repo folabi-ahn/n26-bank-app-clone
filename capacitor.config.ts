67b5531e0eaf3546643027934de3a865b8ed1cba
AHOUNOU Folabi <folabi.ahounou@hatec.tech>
2023-01-14 23:41:16 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'n26-bank-app-clone',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
